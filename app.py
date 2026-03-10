0e8af2afb155ff532df4adea3d118ea25ea6d46a
bhutesoham <bhutesoham@gmail.com>
2026-03-10 20:59:13 +0100

    modified

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -11,6 +10,3 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
-
-
-app.run(debug=True, host='0.0.0.0', port=80)
